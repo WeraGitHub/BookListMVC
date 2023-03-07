1e706ef0e5703055b0bc82dc8a1179928b42af54
Weronika <weronikalimberger@gmail.com>
2023-03-07 12:56:06 +0000

    Add Books Controller and empty View

---
+++ BookListMVC/BookListMVC/Controllers/BooksController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BookListMVC.Controllers
+{
+    public class BooksController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
